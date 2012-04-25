e15b7cee65e5e8e60283009b45355a21f82d6aa5
kobenland <obenland@gmx.de>
2012-04-25 18:08:04 +0200

    Move Author Bio to its own section and reduce whitespace a bit

--- partials/content-single.php
+++ partials/content-single.php
@@ -29,27 +29,26 @@
 		the_tags(
 			'<br class="sep" /><span class="tag-links">' . __( 'Tagged ', 'the-bootstrap' ) . '</span>',
 			_x( ', ', 'used between list items, there is a space after the comma', 'the-bootstrap' )
-		);
-		
-		if ( get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
-		<div id="author-info" class="row">
-			<h2 class="span8"><?php printf( __( 'About %s', 'the-bootstrap' ), get_the_author() ); ?></h2>
-			<div id="author-avatar" class="span1">
-				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'the_bootstrap_author_bio_avatar_size', 70 ) ); ?>
-			</div><!-- #author-avatar -->
-			<div id="author-description" class="span7">
-				<?php the_author_meta( 'description' ); ?>
-				<div id="author-link">
-					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ), get_the_author() ); ?>
-					</a>
-				</div><!-- #author-link	-->
-			</div><!-- #author-description -->
-		</div><!-- #author-info -->
-		<?php endif; ?>
+		); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
-<?php
+
+<?php if ( get_the_author_meta( 'description' ) AND is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
+<aside id="author-info" class="row">
+	<h2 class="span8"><?php printf( __( 'About %s', 'the-bootstrap' ), get_the_author() ); ?></h2>
+	<div id="author-avatar" class="span1">
+		<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'the_bootstrap_author_bio_avatar_size', 70 ) ); ?>
+	</div><!-- #author-avatar -->
+	<div id="author-description" class="span7">
+		<?php the_author_meta( 'description' ); ?>
+		<div id="author-link">
+			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+				<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ), get_the_author() ); ?>
+			</a>
+		</div><!-- #author-link	-->
+	</div><!-- #author-description -->
+</aside><!-- #author-info -->
+<?php endif;
 
 
 /* End of file content-single.php */
