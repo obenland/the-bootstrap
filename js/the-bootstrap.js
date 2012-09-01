643d10668b4a129daaf9e3178c4cf01a595e6921
kobenland <obenland@gmx.de>
2012-09-01 12:40:50 +0200

    Minify it

--- js/the-bootstrap.js
+++ js/the-bootstrap.js
@@ -1,15 +1,12 @@
 jQuery(function($){
-	$('table').addClass('table table-striped table-hover');
-	$('#submit').addClass('btn btn-primary btn-large');
-	$('#wp-calendar').addClass('table table-striped table-bordered');
+	$( 'table' ).addClass( 'table table-striped table-hover' );
+	$( '#submit' ).addClass( 'btn btn-primary btn-large' );
+	$( '#wp-calendar' ).addClass( 'table table-striped table-bordered' );
 	
 	// Bootstrap plugins
-	$('#content [rel="tooltip"]').tooltip();
-	$('#content [rel="popover"]').popover();
-	$('.alert').alert();
-	$('.carousel-inner figure:first-child').addClass('active');
-	$('.carousel').carousel();
-	$('#menu-alert').bind('closed', function(){
-		$.post(the_bootstrap.ajax_url, {action:'the_bootstrap_dismiss_menu'});
-	});
+	$( '#content [rel="tooltip"]' ).tooltip();
+	$( '#content [rel="popover"]' ).popover();
+	$( '.alert' ).alert();
+	$( '.carousel-inner figure:first-child' ).addClass( 'active' );
+	$( '.carousel' ).carousel();
 });
