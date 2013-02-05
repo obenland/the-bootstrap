c677594d9d8c6340099c24069fd40e3727ec83a7
jmjf <jamee.mikell@gmail.com>
2013-02-05 13:48:09 -0500

    Update 404.php
    Add HTML5.1 main tag in place of div for role="main"

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header(); ?>
 <section id="primary" class="span8">
 
 	<?php tha_content_before(); ?>
-	<div id="content" role="main">
+	<main id="content" role="main">
 		<?php tha_content_top(); ?>
 		
 		<?php tha_entry_before(); ?>
@@ -51,7 +51,7 @@ get_header(); ?>
 		<?php tha_entry_after(); ?>
 		
 		<?php tha_content_bottom(); ?>
-	</div><!-- #content -->
+	</main><!-- #content -->
 	<?php tha_content_after(); ?>
 </section><!-- #primary -->
 
